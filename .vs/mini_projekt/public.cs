51ddc4fad58c519b9ba4e86cd2a184694ea0ef48
Rok Žerdoner <rok.zerdoner@scv.si>
2021-01-12 12:18:42 +0100

    public id

---
+++ .vs/mini_projekt/public.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mini_projekt
+{
+    class Public
+    {
+        public static int id = 0;
+        public static void Change(int a)
+        {
+            id = a; 
+        }
+    }
+}
