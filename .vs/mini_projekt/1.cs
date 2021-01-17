47d0aae702fcb552b4d844cb279a7b76a3706cc5
Rok Žerdoner <rok.zerdoner@scv.si>
2021-01-17 10:38:01 +0100

    Izpis porabe denarja

---
+++ .vs/mini_projekt/1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace mini_projekt
+{
+    public static class Ena
+    {
+        public static bool Registracija(string a, string b, string c, string d){
+            bool x = false;
+            using (NpgsqlConnection con = new NpgsqlConnection("Server=hattie.db.elephantsql.com; User Id=qrallryw;" + "Password=42JSx-SoQO5TfgzavjTAU5Bz2qJli0rN; Database=qrallryw;"))
+            {
+                con.Open();
+                NpgsqlCommand com = new NpgsqlCommand("SELECT add_uporabnik1('" + a + "','" + b + "','" + c + "','" + Ena.CreateMD5(d) + "')", con);
+                NpgsqlDataReader reader = com.ExecuteReader();
+                while (reader.Read())
+                {
+                    Public.Change(reader.GetInt32(0));
+                    x = true;
+                    //comboBox1.Items.Add(ime);
+                    //comboBox1.Items.Add(a);
+                }
+                con.Close();
+            }
+            return x;
+        }
+
+        public static bool Prijava(string a, string b)
+        {
+            bool x = false;
+            using (NpgsqlConnection con = new NpgsqlConnection("Server=hattie.db.elephantsql.com; User Id=qrallryw;" + "Password=42JSx-SoQO5TfgzavjTAU5Bz2qJli0rN; Database=qrallryw;"))
+            {
+                con.Open();
+                NpgsqlCommand com = new NpgsqlCommand("SELECT prijava1('" + a + "','" + CreateMD5(b) + "')", con);
+                NpgsqlDataReader reader = com.ExecuteReader();
+                while (reader.Read())
+                {
+                    Public.Change(reader.GetInt32(0));
+                    x = true;
+                    //comboBox1.Items.Add(ime);
+                    //comboBox1.Items.Add(a);
+                }
+                con.Close();
+                return x;
+            }
+            
+        }
+        public static string CreateMD5(string input)
+        {
+            // Use input string to calculate MD5 hash
+            using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+            {
+                byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                // Convert the byte array to hexadecimal string
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    sb.Append(hashBytes[i].ToString("X2"));
+                }
+                return sb.ToString();
+            }
+        }
+    }
+
+
+}
--- .vs/mini_projekt/MainForm.cs
+++ .vs/mini_projekt/MainForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Npgsql;
 
 namespace mini_projekt
 {
@@ -15,7 +16,7 @@ namespace mini_projekt
         public MainForm()
         {
             InitializeComponent();
-           // MessageBox.Show(Convert.ToString(Public.id));
+            Porabe_denarja();
         }
 
         private void DodajButton_Click(object sender, EventArgs e)
@@ -24,6 +25,26 @@ namespace mini_projekt
             var InsertForm = new InsertForm();
             InsertForm.Closed += (s, args) => this.Close();
             InsertForm.Show();
+        }
+
+        private void Porabe_denarja()
+        {
+            int a = 1;
+            listView1.Items.Clear();
+            using (NpgsqlConnection con = new NpgsqlConnection("Server=hattie.db.elephantsql.com; User Id=qrallryw;" + "Password=42JSx-SoQO5TfgzavjTAU5Bz2qJli0rN; Database=qrallryw;"))
+            {
+                con.Open();
+                NpgsqlCommand com = new NpgsqlCommand("SELECT * FROM return_vse_porabe_denarja(" + Public.id + ")", con);
+                NpgsqlDataReader reader = com.ExecuteReader();
+                while (reader.Read())
+                {
+                    var row = new string[] {Convert.ToString(a), Convert.ToString(reader.GetTimeStamp(0)), reader.GetString(1), Convert.ToString(reader.GetInt32(2)) };
+                    var lvl = new ListViewItem(row);
+                    listView1.Items.Add(lvl);
+                    a++;
+                }
+                con.Close();
+            }
         }
     }
 }
