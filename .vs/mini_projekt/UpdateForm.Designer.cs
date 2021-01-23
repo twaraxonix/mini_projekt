ea3c7c1733569bc08c915e02ee6c25ba664c91b0
Rok Žerdoner <rok.zerdoner@scv.si>
2021-01-23 11:07:19 +0100

    začetek update foruma

--- .vs/mini_projekt/UpdateForm.Designer.cs
+++ .vs/mini_projekt/UpdateForm.Designer.cs
@@ -37,6 +37,22 @@
             this.SpremeniLabel = new System.Windows.Forms.Label();
             this.NazajButton = new System.Windows.Forms.Button();
             this.DodajButton = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.NazajButton2 = new System.Windows.Forms.Button();
+            this.DodajLokacijo2 = new System.Windows.Forms.Button();
+            this.ImeKrajaConboBox = new System.Windows.Forms.ComboBox();
+            this.PostnaStevilkaGroupBox = new System.Windows.Forms.ComboBox();
+            this.NaslovLokacijeTextBox = new System.Windows.Forms.TextBox();
+            this.ImeLokacijeTextBox = new System.Windows.Forms.TextBox();
+            this.NaslovLokacije = new System.Windows.Forms.Label();
+            this.ImeLokacijeLabel = new System.Windows.Forms.Label();
+            this.PostnaStevilkaLabel = new System.Windows.Forms.Label();
+            this.ImeKrajaLabel = new System.Windows.Forms.Label();
+            this.DodajLokacijoLabel = new System.Windows.Forms.Label();
+            this.DodadLokacijoButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // ZnesekTextBox
@@ -109,6 +125,7 @@
             this.NazajButton.TabIndex = 15;
             this.NazajButton.Text = "Nazaj";
             this.NazajButton.UseVisualStyleBackColor = true;
+            this.NazajButton.Click += new System.EventHandler(this.NazajButton_Click);
             // 
             // DodajButton
             // 
@@ -118,12 +135,162 @@
             this.DodajButton.TabIndex = 14;
             this.DodajButton.Text = "Dodaj";
             this.DodajButton.UseVisualStyleBackColor = true;
+            this.DodajButton.Click += new System.EventHandler(this.DodajButton_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(68, 317);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 19;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(101, 289);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 17);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Kraj";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.NazajButton2);
+            this.groupBox1.Controls.Add(this.DodajLokacijo2);
+            this.groupBox1.Controls.Add(this.ImeKrajaConboBox);
+            this.groupBox1.Controls.Add(this.PostnaStevilkaGroupBox);
+            this.groupBox1.Controls.Add(this.NaslovLokacijeTextBox);
+            this.groupBox1.Controls.Add(this.ImeLokacijeTextBox);
+            this.groupBox1.Controls.Add(this.NaslovLokacije);
+            this.groupBox1.Controls.Add(this.ImeLokacijeLabel);
+            this.groupBox1.Controls.Add(this.PostnaStevilkaLabel);
+            this.groupBox1.Controls.Add(this.ImeKrajaLabel);
+            this.groupBox1.Controls.Add(this.DodajLokacijoLabel);
+            this.groupBox1.Location = new System.Drawing.Point(688, 57);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(314, 364);
+            this.groupBox1.TabIndex = 20;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // NazajButton2
+            // 
+            this.NazajButton2.Location = new System.Drawing.Point(223, 324);
+            this.NazajButton2.Name = "NazajButton2";
+            this.NazajButton2.Size = new System.Drawing.Size(75, 23);
+            this.NazajButton2.TabIndex = 15;
+            this.NazajButton2.Text = "Nazaj";
+            this.NazajButton2.UseVisualStyleBackColor = true;
+            this.NazajButton2.Click += new System.EventHandler(this.NazajButton2_Click);
+            // 
+            // DodajLokacijo2
+            // 
+            this.DodajLokacijo2.Location = new System.Drawing.Point(107, 324);
+            this.DodajLokacijo2.Name = "DodajLokacijo2";
+            this.DodajLokacijo2.Size = new System.Drawing.Size(75, 23);
+            this.DodajLokacijo2.TabIndex = 14;
+            this.DodajLokacijo2.Text = "Dodaj";
+            this.DodajLokacijo2.UseVisualStyleBackColor = true;
+            this.DodajLokacijo2.Click += new System.EventHandler(this.DodajLokacijo2_Click);
+            // 
+            // ImeKrajaConboBox
+            // 
+            this.ImeKrajaConboBox.FormattingEnabled = true;
+            this.ImeKrajaConboBox.Location = new System.Drawing.Point(51, 94);
+            this.ImeKrajaConboBox.Name = "ImeKrajaConboBox";
+            this.ImeKrajaConboBox.Size = new System.Drawing.Size(220, 24);
+            this.ImeKrajaConboBox.TabIndex = 13;
+            this.ImeKrajaConboBox.SelectedIndexChanged += new System.EventHandler(this.ImeKrajaConboBox_SelectedIndexChanged);
+            // 
+            // PostnaStevilkaGroupBox
+            // 
+            this.PostnaStevilkaGroupBox.FormattingEnabled = true;
+            this.PostnaStevilkaGroupBox.Location = new System.Drawing.Point(51, 141);
+            this.PostnaStevilkaGroupBox.Name = "PostnaStevilkaGroupBox";
+            this.PostnaStevilkaGroupBox.Size = new System.Drawing.Size(220, 24);
+            this.PostnaStevilkaGroupBox.TabIndex = 12;
+            // 
+            // NaslovLokacijeTextBox
+            // 
+            this.NaslovLokacijeTextBox.Location = new System.Drawing.Point(51, 267);
+            this.NaslovLokacijeTextBox.Name = "NaslovLokacijeTextBox";
+            this.NaslovLokacijeTextBox.Size = new System.Drawing.Size(220, 22);
+            this.NaslovLokacijeTextBox.TabIndex = 11;
+            // 
+            // ImeLokacijeTextBox
+            // 
+            this.ImeLokacijeTextBox.Location = new System.Drawing.Point(51, 209);
+            this.ImeLokacijeTextBox.Name = "ImeLokacijeTextBox";
+            this.ImeLokacijeTextBox.Size = new System.Drawing.Size(220, 22);
+            this.ImeLokacijeTextBox.TabIndex = 7;
+            // 
+            // NaslovLokacije
+            // 
+            this.NaslovLokacije.AutoSize = true;
+            this.NaslovLokacije.Location = new System.Drawing.Point(104, 247);
+            this.NaslovLokacije.Name = "NaslovLokacije";
+            this.NaslovLokacije.Size = new System.Drawing.Size(107, 17);
+            this.NaslovLokacije.TabIndex = 4;
+            this.NaslovLokacije.Text = "Naslov Lokacije";
+            // 
+            // ImeLokacijeLabel
+            // 
+            this.ImeLokacijeLabel.AutoSize = true;
+            this.ImeLokacijeLabel.Location = new System.Drawing.Point(113, 180);
+            this.ImeLokacijeLabel.Name = "ImeLokacijeLabel";
+            this.ImeLokacijeLabel.Size = new System.Drawing.Size(86, 17);
+            this.ImeLokacijeLabel.TabIndex = 3;
+            this.ImeLokacijeLabel.Text = "Ime Lokacije";
+            // 
+            // PostnaStevilkaLabel
+            // 
+            this.PostnaStevilkaLabel.AutoSize = true;
+            this.PostnaStevilkaLabel.Location = new System.Drawing.Point(104, 121);
+            this.PostnaStevilkaLabel.Name = "PostnaStevilkaLabel";
+            this.PostnaStevilkaLabel.Size = new System.Drawing.Size(103, 17);
+            this.PostnaStevilkaLabel.TabIndex = 2;
+            this.PostnaStevilkaLabel.Text = "Poštna številka";
+            // 
+            // ImeKrajaLabel
+            // 
+            this.ImeKrajaLabel.AutoSize = true;
+            this.ImeKrajaLabel.Location = new System.Drawing.Point(120, 68);
+            this.ImeKrajaLabel.Name = "ImeKrajaLabel";
+            this.ImeKrajaLabel.Size = new System.Drawing.Size(65, 17);
+            this.ImeKrajaLabel.TabIndex = 1;
+            this.ImeKrajaLabel.Text = "Ime kraja";
+            // 
+            // DodajLokacijoLabel
+            // 
+            this.DodajLokacijoLabel.AutoSize = true;
+            this.DodajLokacijoLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.DodajLokacijoLabel.Location = new System.Drawing.Point(74, 27);
+            this.DodajLokacijoLabel.Name = "DodajLokacijoLabel";
+            this.DodajLokacijoLabel.Size = new System.Drawing.Size(154, 25);
+            this.DodajLokacijoLabel.TabIndex = 0;
+            this.DodajLokacijoLabel.Text = "Dodaj Lokacijo";
+            // 
+            // DodadLokacijoButton
+            // 
+            this.DodadLokacijoButton.Location = new System.Drawing.Point(554, 312);
+            this.DodadLokacijoButton.Name = "DodadLokacijoButton";
+            this.DodadLokacijoButton.Size = new System.Drawing.Size(128, 34);
+            this.DodadLokacijoButton.TabIndex = 21;
+            this.DodadLokacijoButton.Text = "Dodaj Lokacijo";
+            this.DodadLokacijoButton.UseVisualStyleBackColor = true;
+            this.DodadLokacijoButton.Click += new System.EventHandler(this.DodadLokacijoButton_Click);
             // 
             // UpdateForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1103, 450);
+            this.Controls.Add(this.DodadLokacijoButton);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.NazajButton);
             this.Controls.Add(this.DodajButton);
             this.Controls.Add(this.SpremeniLabel);
@@ -135,6 +302,8 @@
             this.Controls.Add(this.DatumLabel);
             this.Name = "UpdateForm";
             this.Text = "UpdateForm";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -151,5 +320,20 @@
         private System.Windows.Forms.Label SpremeniLabel;
         private System.Windows.Forms.Button NazajButton;
         private System.Windows.Forms.Button DodajButton;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button NazajButton2;
+        private System.Windows.Forms.Button DodajLokacijo2;
+        private System.Windows.Forms.ComboBox ImeKrajaConboBox;
+        private System.Windows.Forms.ComboBox PostnaStevilkaGroupBox;
+        private System.Windows.Forms.TextBox NaslovLokacijeTextBox;
+        private System.Windows.Forms.TextBox ImeLokacijeTextBox;
+        private System.Windows.Forms.Label NaslovLokacije;
+        private System.Windows.Forms.Label ImeLokacijeLabel;
+        private System.Windows.Forms.Label PostnaStevilkaLabel;
+        private System.Windows.Forms.Label ImeKrajaLabel;
+        private System.Windows.Forms.Label DodajLokacijoLabel;
+        private System.Windows.Forms.Button DodadLokacijoButton;
     }
 }
--- .vs/mini_projekt/UpdateForm.cs
+++ .vs/mini_projekt/UpdateForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Npgsql;
 
 namespace mini_projekt
 {
@@ -15,6 +16,105 @@ namespace mini_projekt
         public UpdateForm()
         {
             InitializeComponent();
+        }
+
+        private void DodajButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Lokacije()
+        {
+            using (NpgsqlConnection con = new NpgsqlConnection("Server=hattie.db.elephantsql.com; User Id=qrallryw;" + "Password=42JSx-SoQO5TfgzavjTAU5Bz2qJli0rN; Database=qrallryw;"))
+            {
+                con.Open();
+                NpgsqlCommand com = new NpgsqlCommand("SELECT * FROM return_vse_lokacije('" + comboBox1.Text + "')", con);
+                NpgsqlDataReader reader = com.ExecuteReader();
+                while (reader.Read())
+                {
+                    string ime = reader.GetString(0);
+                    //string ime_l = reader.GetString(1);
+                    //string skupaj = ime +", "+ ime_l;
+                    LokacijaComboBox.Items.Add(ime);
+                }
+                con.Close();
+            }
+        }
+
+        private void Kraji()
+        {
+            using (NpgsqlConnection con = new NpgsqlConnection("Server=hattie.db.elephantsql.com; User Id=qrallryw;" + "Password=42JSx-SoQO5TfgzavjTAU5Bz2qJli0rN; Database=qrallryw;"))
+            {
+                con.Open();
+                NpgsqlCommand com = new NpgsqlCommand("SELECT * FROM return_vsi_kraji()", con);
+                NpgsqlDataReader reader = com.ExecuteReader();
+                while (reader.Read())
+                {
+                    ImeKrajaConboBox.Items.Add(reader.GetString(0));
+                    comboBox1.Items.Add(reader.GetString(0));
+                }
+                con.Close();
+            }
+        }
+
+        private void Posta()
+        {
+            using (NpgsqlConnection con = new NpgsqlConnection("Server=hattie.db.elephantsql.com; User Id=qrallryw;" + "Password=42JSx-SoQO5TfgzavjTAU5Bz2qJli0rN; Database=qrallryw;"))
+            {
+                con.Open();
+                NpgsqlCommand com = new NpgsqlCommand("SELECT * FROM return_posto_krajev('" + ImeKrajaConboBox.Text + "')", con);
+                NpgsqlDataReader reader = com.ExecuteReader();
+                while (reader.Read())
+                {
+                    PostnaStevilkaGroupBox.Items.Add(reader.GetString(0));
+                }
+                con.Close();
+            }
+        }
+
+        private void NazajButton_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            var MainForm = new MainForm();
+            MainForm.Closed += (s, args) => this.Close();
+            MainForm.Show();
+        }
+
+        private void DodadLokacijoButton_Click(object sender, EventArgs e)
+        {
+            groupBox1.Visible = true;
+            //Kraji();
+        }
+
+        private void ImeKrajaConboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            PostnaStevilkaGroupBox.Items.Clear();
+            Posta();
+        }
+
+        private void DodajLokacijo2_Click(object sender, EventArgs e)
+        {
+            using (NpgsqlConnection con = new NpgsqlConnection("Server=hattie.db.elephantsql.com; User Id=qrallryw;" + "Password=42JSx-SoQO5TfgzavjTAU5Bz2qJli0rN; Database=qrallryw;"))
+            {
+                con.Open();
+                NpgsqlCommand com = new NpgsqlCommand("SELECT add_lokacija('" + ImeKrajaConboBox.Text + "','" + PostnaStevilkaGroupBox.Text + "','" + ImeLokacijeTextBox.Text + "','" + NaslovLokacijeTextBox.Text + "')", con);
+                NpgsqlDataReader reader = com.ExecuteReader();
+                while (reader.Read())
+                {
+                }
+                con.Close();
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            LokacijaComboBox.Items.Clear();
+            Lokacije();
+        }
+
+        private void NazajButton2_Click(object sender, EventArgs e)
+        {
+            groupBox1.Visible = false;
         }
     }
 }
