f58f4f47a01f7cb3b6cde7df4542019ed5168594
Rok Žerdoner <rok.zerdoner@scv.si>
2021-01-31 17:46:32 +0100

    Main forum lepotni popravki

--- .vs/mini_projekt/MainForm.Designer.cs
+++ .vs/mini_projekt/MainForm.Designer.cs
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.ListViewGroup listViewGroup2 = new System.Windows.Forms.ListViewGroup("ListViewGroup", System.Windows.Forms.HorizontalAlignment.Left);
+            System.Windows.Forms.ListViewGroup listViewGroup1 = new System.Windows.Forms.ListViewGroup("ListViewGroup", System.Windows.Forms.HorizontalAlignment.Left);
             this.listView1 = new System.Windows.Forms.ListView();
             this.StevilkaColumnHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.DatumColumnHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
@@ -53,10 +53,10 @@
             this.LokacijaColumnHeader,
             this.ZnesekColumnHeader});
             this.listView1.FullRowSelect = true;
-            listViewGroup2.Header = "ListViewGroup";
-            listViewGroup2.Name = "StevilkaListViewGroup1";
+            listViewGroup1.Header = "ListViewGroup";
+            listViewGroup1.Name = "StevilkaListViewGroup1";
             this.listView1.Groups.AddRange(new System.Windows.Forms.ListViewGroup[] {
-            listViewGroup2});
+            listViewGroup1});
             this.listView1.HideSelection = false;
             this.listView1.Location = new System.Drawing.Point(57, 75);
             this.listView1.Name = "listView1";
@@ -90,6 +90,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
             this.label1.Location = new System.Drawing.Point(310, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(241, 44);
@@ -98,79 +99,128 @@
             // 
             // DodajButton
             // 
-            this.DodajButton.Location = new System.Drawing.Point(124, 372);
+            this.DodajButton.BackColor = System.Drawing.Color.RoyalBlue;
+            this.DodajButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.DodajButton.FlatAppearance.BorderSize = 3;
+            this.DodajButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DodgerBlue;
+            this.DodajButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DodgerBlue;
+            this.DodajButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.DodajButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.DodajButton.ForeColor = System.Drawing.SystemColors.Control;
+            this.DodajButton.Location = new System.Drawing.Point(919, 193);
             this.DodajButton.Name = "DodajButton";
-            this.DodajButton.Size = new System.Drawing.Size(119, 41);
+            this.DodajButton.Size = new System.Drawing.Size(158, 53);
             this.DodajButton.TabIndex = 2;
             this.DodajButton.Text = "Dodaj";
-            this.DodajButton.UseVisualStyleBackColor = true;
+            this.DodajButton.UseVisualStyleBackColor = false;
             this.DodajButton.Click += new System.EventHandler(this.DodajButton_Click);
             // 
             // ArhivButton
             // 
-            this.ArhivButton.Location = new System.Drawing.Point(318, 372);
+            this.ArhivButton.BackColor = System.Drawing.Color.RoyalBlue;
+            this.ArhivButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.ArhivButton.FlatAppearance.BorderSize = 3;
+            this.ArhivButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DodgerBlue;
+            this.ArhivButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DodgerBlue;
+            this.ArhivButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ArhivButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.ArhivButton.ForeColor = System.Drawing.SystemColors.Control;
+            this.ArhivButton.Location = new System.Drawing.Point(919, 262);
             this.ArhivButton.Name = "ArhivButton";
-            this.ArhivButton.Size = new System.Drawing.Size(122, 41);
+            this.ArhivButton.Size = new System.Drawing.Size(158, 48);
             this.ArhivButton.TabIndex = 3;
             this.ArhivButton.Text = "Arhiv";
-            this.ArhivButton.UseVisualStyleBackColor = true;
+            this.ArhivButton.UseVisualStyleBackColor = false;
             this.ArhivButton.Click += new System.EventHandler(this.ArhivButton_Click);
             // 
             // UpdateButton
             // 
-            this.UpdateButton.Location = new System.Drawing.Point(642, 326);
+            this.UpdateButton.BackColor = System.Drawing.Color.RoyalBlue;
+            this.UpdateButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.UpdateButton.FlatAppearance.BorderSize = 3;
+            this.UpdateButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DodgerBlue;
+            this.UpdateButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DodgerBlue;
+            this.UpdateButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.UpdateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.UpdateButton.ForeColor = System.Drawing.SystemColors.Control;
+            this.UpdateButton.Location = new System.Drawing.Point(535, 327);
             this.UpdateButton.Name = "UpdateButton";
-            this.UpdateButton.Size = new System.Drawing.Size(104, 46);
+            this.UpdateButton.Size = new System.Drawing.Size(150, 45);
             this.UpdateButton.TabIndex = 4;
             this.UpdateButton.Text = "Update";
-            this.UpdateButton.UseVisualStyleBackColor = true;
+            this.UpdateButton.UseVisualStyleBackColor = false;
             this.UpdateButton.Click += new System.EventHandler(this.UpdateButton_Click);
             // 
             // DeleteButton
             // 
-            this.DeleteButton.Location = new System.Drawing.Point(749, 326);
+            this.DeleteButton.BackColor = System.Drawing.Color.RoyalBlue;
+            this.DeleteButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.DeleteButton.FlatAppearance.BorderSize = 3;
+            this.DeleteButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DodgerBlue;
+            this.DeleteButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DodgerBlue;
+            this.DeleteButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.DeleteButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.DeleteButton.ForeColor = System.Drawing.SystemColors.Control;
+            this.DeleteButton.Location = new System.Drawing.Point(691, 327);
             this.DeleteButton.Name = "DeleteButton";
-            this.DeleteButton.Size = new System.Drawing.Size(105, 46);
+            this.DeleteButton.Size = new System.Drawing.Size(154, 45);
             this.DeleteButton.TabIndex = 5;
             this.DeleteButton.Text = "Delete";
-            this.DeleteButton.UseVisualStyleBackColor = true;
+            this.DeleteButton.UseVisualStyleBackColor = false;
             this.DeleteButton.Click += new System.EventHandler(this.DeleteButton_Click);
             // 
             // IzvoziButton
             // 
-            this.IzvoziButton.Location = new System.Drawing.Point(860, 326);
+            this.IzvoziButton.BackColor = System.Drawing.Color.RoyalBlue;
+            this.IzvoziButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.IzvoziButton.FlatAppearance.BorderSize = 3;
+            this.IzvoziButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DodgerBlue;
+            this.IzvoziButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DodgerBlue;
+            this.IzvoziButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.IzvoziButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.IzvoziButton.ForeColor = System.Drawing.SystemColors.Control;
+            this.IzvoziButton.Location = new System.Drawing.Point(379, 327);
             this.IzvoziButton.Name = "IzvoziButton";
-            this.IzvoziButton.Size = new System.Drawing.Size(103, 46);
+            this.IzvoziButton.Size = new System.Drawing.Size(150, 46);
             this.IzvoziButton.TabIndex = 6;
             this.IzvoziButton.Text = "Izvozi";
-            this.IzvoziButton.UseVisualStyleBackColor = true;
+            this.IzvoziButton.UseVisualStyleBackColor = false;
             this.IzvoziButton.Click += new System.EventHandler(this.IzvoziButton_Click);
             // 
             // OsebniPodatkiButton
             // 
-            this.OsebniPodatkiButton.Location = new System.Drawing.Point(476, 372);
+            this.OsebniPodatkiButton.BackColor = System.Drawing.Color.RoyalBlue;
+            this.OsebniPodatkiButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.OsebniPodatkiButton.FlatAppearance.BorderSize = 3;
+            this.OsebniPodatkiButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DodgerBlue;
+            this.OsebniPodatkiButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DodgerBlue;
+            this.OsebniPodatkiButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.OsebniPodatkiButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.OsebniPodatkiButton.ForeColor = System.Drawing.SystemColors.Control;
+            this.OsebniPodatkiButton.Location = new System.Drawing.Point(919, 326);
             this.OsebniPodatkiButton.Name = "OsebniPodatkiButton";
-            this.OsebniPodatkiButton.Size = new System.Drawing.Size(115, 41);
+            this.OsebniPodatkiButton.Size = new System.Drawing.Size(158, 46);
             this.OsebniPodatkiButton.TabIndex = 7;
             this.OsebniPodatkiButton.Text = "Osebni Podatki";
-            this.OsebniPodatkiButton.UseVisualStyleBackColor = true;
+            this.OsebniPodatkiButton.UseVisualStyleBackColor = false;
             this.OsebniPodatkiButton.Click += new System.EventHandler(this.OsebniPodatkiButton_Click);
             // 
             // SkupajTextBox
             // 
             this.SkupajTextBox.Enabled = false;
-            this.SkupajTextBox.Location = new System.Drawing.Point(861, 137);
+            this.SkupajTextBox.Location = new System.Drawing.Point(919, 120);
             this.SkupajTextBox.Name = "SkupajTextBox";
-            this.SkupajTextBox.Size = new System.Drawing.Size(100, 22);
+            this.SkupajTextBox.Size = new System.Drawing.Size(158, 22);
             this.SkupajTextBox.TabIndex = 8;
             // 
             // SkupajLabel
             // 
             this.SkupajLabel.AutoSize = true;
-            this.SkupajLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.SkupajLabel.Location = new System.Drawing.Point(874, 91);
+            this.SkupajLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.SkupajLabel.ForeColor = System.Drawing.SystemColors.Control;
+            this.SkupajLabel.Location = new System.Drawing.Point(958, 75);
             this.SkupajLabel.Name = "SkupajLabel";
-            this.SkupajLabel.Size = new System.Drawing.Size(73, 24);
+            this.SkupajLabel.Size = new System.Drawing.Size(79, 25);
             this.SkupajLabel.TabIndex = 9;
             this.SkupajLabel.Text = "Skupaj";
             // 
@@ -178,6 +228,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MidnightBlue;
             this.ClientSize = new System.Drawing.Size(1191, 450);
             this.Controls.Add(this.SkupajLabel);
             this.Controls.Add(this.SkupajTextBox);
