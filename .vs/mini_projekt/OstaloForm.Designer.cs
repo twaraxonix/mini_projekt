005eb5b355d62ca7a71d61acce62d4c41c79035f
Rok Žerdoner <rok.zerdoner@scv.si>
2021-02-08 14:53:25 +0100

    lepotni popravki v OstaloForumu

--- .vs/mini_projekt/OstaloForm.Designer.cs
+++ .vs/mini_projekt/OstaloForm.Designer.cs
@@ -90,69 +90,113 @@
             // 
             // NazajButton
             // 
-            this.NazajButton.Location = new System.Drawing.Point(37, 476);
+            this.NazajButton.BackColor = System.Drawing.Color.RoyalBlue;
+            this.NazajButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.NazajButton.FlatAppearance.BorderSize = 3;
+            this.NazajButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DodgerBlue;
+            this.NazajButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DodgerBlue;
+            this.NazajButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.NazajButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.NazajButton.ForeColor = System.Drawing.Color.White;
+            this.NazajButton.Location = new System.Drawing.Point(37, 458);
             this.NazajButton.Name = "NazajButton";
-            this.NazajButton.Size = new System.Drawing.Size(81, 31);
+            this.NazajButton.Size = new System.Drawing.Size(137, 49);
             this.NazajButton.TabIndex = 1;
             this.NazajButton.Text = "Nazaj";
-            this.NazajButton.UseVisualStyleBackColor = true;
+            this.NazajButton.UseVisualStyleBackColor = false;
             this.NazajButton.Click += new System.EventHandler(this.NazajButton_Click);
             // 
             // DeleteButton
             // 
+            this.DeleteButton.BackColor = System.Drawing.Color.RoyalBlue;
+            this.DeleteButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.DeleteButton.FlatAppearance.BorderSize = 3;
+            this.DeleteButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DodgerBlue;
+            this.DeleteButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DodgerBlue;
+            this.DeleteButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.DeleteButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.DeleteButton.ForeColor = System.Drawing.Color.White;
             this.DeleteButton.Location = new System.Drawing.Point(380, 358);
             this.DeleteButton.Name = "DeleteButton";
-            this.DeleteButton.Size = new System.Drawing.Size(101, 54);
+            this.DeleteButton.Size = new System.Drawing.Size(138, 54);
             this.DeleteButton.TabIndex = 2;
             this.DeleteButton.Text = "Izbriši";
-            this.DeleteButton.UseVisualStyleBackColor = true;
+            this.DeleteButton.UseVisualStyleBackColor = false;
             this.DeleteButton.Click += new System.EventHandler(this.DeleteButton_Click);
             // 
             // KrajiButton
             // 
+            this.KrajiButton.BackColor = System.Drawing.Color.RoyalBlue;
+            this.KrajiButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.KrajiButton.FlatAppearance.BorderSize = 3;
+            this.KrajiButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DodgerBlue;
+            this.KrajiButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DodgerBlue;
+            this.KrajiButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.KrajiButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.KrajiButton.ForeColor = System.Drawing.Color.White;
             this.KrajiButton.Location = new System.Drawing.Point(37, 358);
             this.KrajiButton.Name = "KrajiButton";
-            this.KrajiButton.Size = new System.Drawing.Size(112, 54);
+            this.KrajiButton.Size = new System.Drawing.Size(137, 54);
             this.KrajiButton.TabIndex = 3;
             this.KrajiButton.Text = "Kraji";
-            this.KrajiButton.UseVisualStyleBackColor = true;
+            this.KrajiButton.UseVisualStyleBackColor = false;
             this.KrajiButton.Click += new System.EventHandler(this.KrajiButton_Click);
             // 
             // LokacijeButton
             // 
+            this.LokacijeButton.BackColor = System.Drawing.Color.RoyalBlue;
+            this.LokacijeButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.LokacijeButton.FlatAppearance.BorderSize = 3;
+            this.LokacijeButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DodgerBlue;
+            this.LokacijeButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DodgerBlue;
+            this.LokacijeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.LokacijeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.LokacijeButton.ForeColor = System.Drawing.Color.White;
             this.LokacijeButton.Location = new System.Drawing.Point(180, 358);
             this.LokacijeButton.Name = "LokacijeButton";
-            this.LokacijeButton.Size = new System.Drawing.Size(95, 54);
+            this.LokacijeButton.Size = new System.Drawing.Size(127, 54);
             this.LokacijeButton.TabIndex = 4;
             this.LokacijeButton.Text = "Lokacije";
-            this.LokacijeButton.UseVisualStyleBackColor = true;
+            this.LokacijeButton.UseVisualStyleBackColor = false;
             this.LokacijeButton.Click += new System.EventHandler(this.LokacijeButton_Click);
             // 
             // VnesiKrajButton
             // 
+            this.VnesiKrajButton.BackColor = System.Drawing.Color.RoyalBlue;
+            this.VnesiKrajButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.VnesiKrajButton.FlatAppearance.BorderSize = 3;
+            this.VnesiKrajButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DodgerBlue;
+            this.VnesiKrajButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DodgerBlue;
+            this.VnesiKrajButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.VnesiKrajButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.VnesiKrajButton.ForeColor = System.Drawing.Color.White;
             this.VnesiKrajButton.Location = new System.Drawing.Point(549, 358);
             this.VnesiKrajButton.Name = "VnesiKrajButton";
-            this.VnesiKrajButton.Size = new System.Drawing.Size(91, 54);
+            this.VnesiKrajButton.Size = new System.Drawing.Size(129, 54);
             this.VnesiKrajButton.TabIndex = 5;
             this.VnesiKrajButton.Text = "Vnesi kraj";
-            this.VnesiKrajButton.UseVisualStyleBackColor = true;
+            this.VnesiKrajButton.UseVisualStyleBackColor = false;
             this.VnesiKrajButton.Click += new System.EventHandler(this.VnesiKrajButton_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.White;
             this.label1.Location = new System.Drawing.Point(106, 111);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.Size = new System.Drawing.Size(70, 25);
             this.label1.TabIndex = 6;
             this.label1.Text = "label1";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.Color.White;
             this.label2.Location = new System.Drawing.Point(84, 189);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.Size = new System.Drawing.Size(70, 25);
             this.label2.TabIndex = 7;
             this.label2.Text = "label2";
             // 
@@ -167,9 +211,11 @@
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.ForeColor = System.Drawing.Color.White;
             this.label3.Location = new System.Drawing.Point(84, 265);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.Size = new System.Drawing.Size(70, 25);
             this.label3.TabIndex = 9;
             this.label3.Text = "label3";
             // 
@@ -206,47 +252,74 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(99, 362);
+            this.button1.BackColor = System.Drawing.Color.RoyalBlue;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button1.FlatAppearance.BorderSize = 3;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DodgerBlue;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DodgerBlue;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(46, 362);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(201, 45);
             this.button1.TabIndex = 12;
             this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.ForeColor = System.Drawing.Color.White;
             this.label4.Location = new System.Drawing.Point(96, 31);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(46, 17);
+            this.label4.Size = new System.Drawing.Size(101, 36);
             this.label4.TabIndex = 13;
             this.label4.Text = "label4";
             // 
             // VnesiLokacijoButton
             // 
+            this.VnesiLokacijoButton.BackColor = System.Drawing.Color.RoyalBlue;
+            this.VnesiLokacijoButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.VnesiLokacijoButton.FlatAppearance.BorderSize = 3;
+            this.VnesiLokacijoButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DodgerBlue;
+            this.VnesiLokacijoButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DodgerBlue;
+            this.VnesiLokacijoButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.VnesiLokacijoButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.VnesiLokacijoButton.ForeColor = System.Drawing.Color.White;
             this.VnesiLokacijoButton.Location = new System.Drawing.Point(549, 418);
             this.VnesiLokacijoButton.Name = "VnesiLokacijoButton";
-            this.VnesiLokacijoButton.Size = new System.Drawing.Size(91, 45);
+            this.VnesiLokacijoButton.Size = new System.Drawing.Size(129, 66);
             this.VnesiLokacijoButton.TabIndex = 14;
             this.VnesiLokacijoButton.Text = "Vnesi lokacijo";
-            this.VnesiLokacijoButton.UseVisualStyleBackColor = true;
+            this.VnesiLokacijoButton.UseVisualStyleBackColor = false;
             this.VnesiLokacijoButton.Click += new System.EventHandler(this.VnesiLokacijoButton_Click);
             // 
             // UpdateButton
             // 
+            this.UpdateButton.BackColor = System.Drawing.Color.RoyalBlue;
+            this.UpdateButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.UpdateButton.FlatAppearance.BorderSize = 3;
+            this.UpdateButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DodgerBlue;
+            this.UpdateButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DodgerBlue;
+            this.UpdateButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.UpdateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.UpdateButton.ForeColor = System.Drawing.Color.White;
             this.UpdateButton.Location = new System.Drawing.Point(380, 418);
             this.UpdateButton.Name = "UpdateButton";
-            this.UpdateButton.Size = new System.Drawing.Size(101, 45);
+            this.UpdateButton.Size = new System.Drawing.Size(138, 55);
             this.UpdateButton.TabIndex = 15;
             this.UpdateButton.Text = "Spremeni";
-            this.UpdateButton.UseVisualStyleBackColor = true;
+            this.UpdateButton.UseVisualStyleBackColor = false;
             this.UpdateButton.Click += new System.EventHandler(this.UpdateButton_Click);
             // 
             // OstaloForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MidnightBlue;
             this.ClientSize = new System.Drawing.Size(1006, 519);
             this.Controls.Add(this.UpdateButton);
             this.Controls.Add(this.VnesiLokacijoButton);
